c68d09f7528f1e7d15f26e740a38ce4f800dffa3
Jeff Putz <jeff@popw.com>
2022-03-09 20:41:43 -0500

    Put some letters on the screen

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Phrazy.Client;
+using Phrazy.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<IGameEngine, GameEngine>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/GameEngine.cs
@@ -0,0 +1,12 @@
+﻿namespace Phrazy.Client.Services
+{
+    public interface IGameEngine
+    {
+        string Phrase { get; set; }
+    }
+
+    public class GameEngine : IGameEngine
+    {
+        public string Phrase { get; set; }
+    }
+}
