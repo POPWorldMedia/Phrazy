5ad868a709d88794b1d8edd5107a59b284e1c211
Jeff Putz <jeff@popw.com>
2022-03-26 00:04:36 -0400

    Create Database.sql

---
+++ src/Server/Database.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Results](
+	[DeviceID] [nvarchar](50) NOT NULL,
+	[PuzzleID] [nvarchar](50) NOT NULL,
+	[Score] [int] NOT NULL,
+	[Rank] [int] NULL,
+	[TimeStamp] [datetime] NOT NULL,
+	[IsWin] [bit] NOT NULL,
+	[Results] [nvarchar](max) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE CLUSTERED INDEX [IX_Results_PuzzleID] ON [dbo].[Results]
+(
+	[PuzzleID] ASC
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Results_DeviceID_PuzzleID] ON [dbo].[Results]
+(
+	[DeviceID] ASC,
+	[PuzzleID] ASC
+)
+GO
+
