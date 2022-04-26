4b1b615a93f9eb8b4a8f00437efecf54a6f7c4f2
Jeff Putz <jeff@popw.com>
2022-04-26 18:46:02 -0400

    PWA plumbing

---
+++ src/Client/wwwroot/sw.js
@@ -0,0 +1,8 @@
+﻿self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+
+async function onInstall(event) {
+	self.skipWaiting();
+}
+
+self.addEventListener('fetch', event => {
+});
