16256c0d33cd7db2da41c6093c7c3e7121fe34d9
Jeff Putz <jeff@popw.com>
2022-04-26 22:59:18 -0400

    wireup sw sync event

--- src/Client/wwwroot/sw.js
+++ src/Client/wwwroot/sw.js
@@ -6,3 +6,12 @@ async function onInstall(event) {
 
 self.addEventListener('fetch', event => {
 });
+
+if ('sync' in self.registration)
+	self.registration.sync.register("dosync");
+
+const broadcast = new BroadcastChannel("p");
+self.addEventListener('sync', function (event) {
+	console.log("sync fired");
+	event.waitUntil(broadcast.postMessage({ payload: "do" }));
+});
